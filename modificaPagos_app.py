09949a6f453d3b25b94fda9b4f3f9852becd8299
unknown <josegerchinhoren97@gmail.com>
2023-09-03 20:41:57 -0300

    Hasta modificacion pagos, falta que cargue los datos en tabla modificacionesPagos

--- modificaPagos_app.py
+++ modificaPagos_app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pyodbc
 import json
-from datetime import datetime
 
 # Cargar configuración desde el archivo config.json
 with open("../config.json") as config_file:
     config = json.load(config_file)
 
-# Función para establecer la conexión a la base de datos
-def conectar_db():
+# Función para conectar a la base de datos
+def conectar_bd():
     return pyodbc.connect(
         driver=config["driver"],
         server=config["server"],
@@ -17,35 +16,32 @@ def conectar_db():
         pwd=config["password"]
     )
 
-# Función para obtener los datos de un cliente por su ID
-def obtener_datos_cliente(id_cliente):
-    conn = conectar_db()
+# Función para obtener los nombres de los clientes
+def obtener_nombres_clientes():
+    conn = conectar_bd()
     cursor = conn.cursor()
     
-    query = """
-    SELECT * FROM Cliente
-    WHERE idCliente = ?
-    """
-    
-    cursor.execute(query, (id_cliente,))
-    row = cursor.fetchone()
+    query = "SELECT nombreApellidoCliente FROM Pago GROUP BY nombreApellidoCliente"
+    cursor.execute(query)
+    nombres_clientes = [cliente[0] for cliente in cursor.fetchall()]
     
     cursor.close()
     conn.close()
     
-    return row  # Devuelve una fila de datos del cliente o None si no se encuentra
+    return nombres_clientes
 
-# Función para obtener los pagos de un cliente por su ID
-def obtener_pagos_cliente(id_cliente):
-    conn = conectar_db()
+# Función para obtener los pagos de un cliente por su nombre y apellido
+def obtener_pagos_cliente(nombre_apellido):
+    conn = conectar_bd()
     cursor = conn.cursor()
     
     query = """
-    SELECT * FROM Pago
-    WHERE idCliente = ?
+    SELECT idPago, fechaPago, montoPago, metodoPago, detallePago
+    FROM Pago
+    WHERE nombreApellidoCliente = ?
     """
     
-    cursor.execute(query, (id_cliente,))
+    cursor.execute(query, (nombre_apellido,))
     rows = cursor.fetchall()
     
     cursor.close()
@@ -53,55 +49,56 @@ def obtener_pagos_cliente(id_cliente):
     
     return rows  # Devuelve una lista de filas de datos de pagos del cliente
 
-# Función para editar un pago por su ID
-def editar_pago(id_pago, fecha_pago, monto, metodo_pago, detalle_pago):
-    conn = conectar_db()
+# Función para obtener el ID de usuario a partir de su nombre de usuario
+def obtener_id_usuario(nombre_usuario):
+    conn = conectar_bd()
     cursor = conn.cursor()
     
     query = """
-    UPDATE Pago
-    SET
-        fecha_pago = ?,
-        monto = ?,
-        metodoPago = ?,
-        detallePago = ?
-    WHERE idPago = ?
+    SELECT idUsuario
+    FROM Usuario
+    WHERE nombre = ?
     """
     
-    values = (fecha_pago, monto, metodo_pago, detalle_pago, id_pago)
-    cursor.execute(query, values)
-    conn.commit()
-    
-    # Registrar la modificación en la tabla ModificacionesPagos
-    usuario_modificacion = st.session_state.user_nombre_apellido  # Nombre y apellido del usuario que realiza la modificación
-    query_modificacion = """
-    INSERT INTO ModificacionesPagos (idPago, usuarioModificacion, fecha_modificacion)
-    VALUES (?, ?, GETDATE())
-    """
-    values_modificacion = (id_pago, usuario_modificacion)
-    cursor.execute(query_modificacion, values_modificacion)
-    conn.commit()
-    
-    cursor.close()
-    conn.close()
-
-# Función para obtener los datos de un pago por su ID
-def obtener_datos_pago(id_pago):
-    conn = conectar_db()
-    cursor = conn.cursor()
-    
-    query = """
-    SELECT * FROM Pago
-    WHERE idPago = ?
-    """
-    
-    cursor.execute(query, (id_pago,))
+    cursor.execute(query, (nombre_usuario,))
     row = cursor.fetchone()
     
     cursor.close()
     conn.close()
     
-    return row  # Devuelve una fila de datos del pago o None si no se encuentra
+    return row[0] if row else None  # Devuelve el ID de usuario o None si no se encuentra
+
+# Función para editar un pago por su ID
+def editar_pago(id_pago, monto, metodo_pago, detalle_pago, nombre_usuario):
+    conn = conectar_bd()
+    cursor = conn.cursor()
+    
+    query = """
+    UPDATE Pago
+    SET
+        montoPago = ?,
+        metodoPago = ?,
+        detallePago = ?
+    WHERE idPago = ?
+    """
+    
+    values = (monto, metodo_pago, detalle_pago, id_pago)
+    cursor.execute(query, values)
+    
+    # Obtener el ID de usuario a partir del nombre de usuario
+    id_usuario_modificacion = obtener_id_usuario(nombre_usuario)
+    
+    # Registrar la modificación en la tabla ModificacionesPagos
+    query_modificacion = """
+    INSERT INTO ModificacionesPagos (idPago, idUsuarioModificacion, fechaModificacion)
+    VALUES (?, ?, GETDATE())
+    """
+    values_modificacion = (id_pago, id_usuario_modificacion)
+    cursor.execute(query_modificacion, values_modificacion)
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def main():
     st.title("Modificar Pagos de Clientes")
@@ -112,67 +109,73 @@ def main():
     st.write("Modificar Pagos")
     
     # Obtener la lista de nombres y apellidos de los clientes desde la base de datos
-    conn = conectar_db()
-    cursor = conn.cursor()
-    
-    query_clientes = "SELECT idCliente, nombre_apellido FROM Cliente"
-    cursor.execute(query_clientes)
-    clientes = cursor.fetchall()
-    
-    cursor.close()
-    conn.close()
+    nombres_clientes = obtener_nombres_clientes()
     
     # Crear un cuadro desplegable para seleccionar un cliente
-    selected_cliente = st.selectbox("Seleccionar Cliente:", clientes, format_func=lambda x: x[1])
+    selected_cliente = st.selectbox("Seleccionar Cliente:", nombres_clientes)
     
     if selected_cliente:
-        id_cliente = selected_cliente[0]
-        nombre_cliente = selected_cliente[1]
-        
         # Obtener los pagos del cliente
-        pagos_cliente = obtener_pagos_cliente(id_cliente)
+        pagos_cliente = obtener_pagos_cliente(selected_cliente)
         
         if pagos_cliente:
-            st.write(f"Pagos de {nombre_cliente}:")
+            st.write(f"Pagos de {selected_cliente}:")
             
             # Mostrar los pagos del cliente en una tabla
             st.write(pagos_cliente)
             
             # Permitir al usuario seleccionar un pago para editar por su ID
             id_pago = st.number_input("ID del Pago a Editar", min_value=1)
-            
+
             if id_pago:
                 # Verificar si el ID del pago ingresado pertenece al cliente
                 if any(pago[0] == id_pago for pago in pagos_cliente):
                     st.write("Información del Pago:")
                     
-                    # Obtener los datos del pago
-                    pago_data = obtener_datos_pago(id_pago)
+                    # Encontrar el pago seleccionado
+                    pago_seleccionado = next(pago for pago in pagos_cliente if pago[0] == id_pago)
+                    id_pago, fecha_pago, monto_pago, metodo_pago, detalle_pago = pago_seleccionado
                     
-                    # Iterar sobre los campos y sus valores
-                    for i in range(len(pago_data.cursor_description)):
-                        campo = pago_data.cursor_description[i][0]  # Nombre del campo desde el cursor
-                        valor = pago_data[i]  # Valor del campo
+                    # Mostrar toda la información del pago
+                    st.write(f"ID de Pago: {id_pago}")
+                    st.write(f"Fecha de Pago: {fecha_pago}")
+                    st.write(f"Monto: {monto_pago}")
+                    st.write(f"Método de Pago: {metodo_pago}")
+                    st.write(f"Detalle de Pago: {detalle_pago}")
+                    
+                    st.write("Editar Campos:")
+                    
+                    # Permitir al usuario seleccionar el campo del pago que desea editar
+                    campos_editables = ["Monto", "Método de Pago", "Detalle de Pago"]
+                    campo_editar = st.selectbox("Seleccione el campo a editar:", campos_editables)
+                    
+                    # Habilitar la edición solo para los campos seleccionados
+                    if campo_editar == "Monto":
+                        nuevo_valor = float(st.number_input("Nuevo Monto", value=float(monto_pago)))
+                    elif campo_editar == "Método de Pago":
+                        nuevo_valor = st.text_input("Nuevo Método de Pago", value=metodo_pago)
+                    elif campo_editar == "Detalle de Pago":
+                        nuevo_valor = st.text_area("Nuevo Detalle de Pago", value=detalle_pago)
+                    else:
+                        nuevo_valor = None
+                    
+                    if st.button("Guardar Cambios") and nuevo_valor is not None:
+                        # Realizar la edición del campo seleccionado
+                        if campo_editar == "Monto":
+                            editar_pago(id_pago, nuevo_valor, metodo_pago, detalle_pago, user_nombre_apellido)
+                        elif campo_editar == "Método de Pago":
+                            editar_pago(id_pago, monto_pago, nuevo_valor, detalle_pago, user_nombre_apellido)
+                        elif campo_editar == "Detalle de Pago":
+                            editar_pago(id_pago, monto_pago, metodo_pago, nuevo_valor, user_nombre_apellido)
                         
-                        # Mostrar cada campo y su valor en una fila
-                        st.write(f"{campo}: {valor}")
-                    
-                    # Permitir al usuario editar los campos del pago
-                    fecha_pago = st.date_input("Fecha de Pago", value=datetime.strptime(pago_data[2], "%Y-%m-%d"))
-                    monto = st.number_input("Monto", value=pago_data[3])
-                    metodo_pago = st.text_input("Método de Pago", value=pago_data[4])
-                    detalle_pago = st.text_area("Detalle de Pago", value=pago_data[5])
-                    
-                    if st.button("Guardar Cambios"):
-                        # Realizar la edición del pago con los nuevos valores
-                        editar_pago(id_pago, fecha_pago, monto, metodo_pago, detalle_pago)
-                        st.success("Se ha editado el pago correctamente.")
+                        st.success(f"Se ha editado el campo {campo_editar} correctamente.")
                 else:
                     st.error("ID de Pago no válido para este cliente.")
             else:
                 st.warning("Ingrese un ID de Pago válido para editar.")
+
         else:
-            st.warning(f"No se encontraron pagos para {nombre_cliente}.")
-    
+            st.warning(f"No se encontraron pagos para {selected_cliente}.")
+
 if __name__ == "__main__":
     main()
