b67696e602fdd8b6f83b2b22bf396baf6caf47a1
unknown <josegerchinhoren97@gmail.com>
2023-09-26 03:12:35 -0300

    Avance de dashboard

--- dashboard.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-streamlit run dashboard_app.py
--- indicadores_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Indicadores")
-    
-    # Aquí puedes agregar gráficos, tablas o visualizaciones para mostrar indicadores.
-
-if __name__ == "__main__":
-    main()
