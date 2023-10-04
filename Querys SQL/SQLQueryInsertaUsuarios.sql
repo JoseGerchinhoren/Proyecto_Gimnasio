ad7b3ea3a9a2db0392a69924aeeaeb5b70ce25f9
unknown <josegerchinhoren97@gmail.com>
2023-10-04 03:30:19 -0300

    Inserte Usuarios en base de datos

---
+++ Querys SQL/SQLQueryInsertaUsuarios.sql
@@ -0,0 +1,4 @@
+-- Insertar el primer usuario
+INSERT INTO Usuario VALUES ('Juan', 'Pérez', 'juan.perez@email.com', 'contrasena1', '1990-01-15', '12345678', 'Calle 123', GETDATE(), 'Personal de atencion al publico', 'usuario')
+, ('María', 'Gómez', 'maria.gomez@email.com', 'contrasena2', '1985-07-20', '87654321', 'Avenida 456', GETDATE(), 'Personal de atencion al publico', 'usuario')
+, ('Carlos', 'López', 'carlos.lopez@email.com', 'contrasena3', '1995-03-10', '98765432', 'Calle 789', GETDATE(), 'Personal de atencion al publico', 'usuario')
